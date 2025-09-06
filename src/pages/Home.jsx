647501d6b91d1ff17e465de6ec1dce3815e5aae7
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2025-09-05 19:20:07 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,57 @@
 import React from 'react';
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <Box sx={{ p: 3 }}>
-      <Typography variant="h3" gutterBottom>Welcome to doGood</Typography>
-      <Typography variant="body1" paragraph>
-        doGood is a charity for the orphaned childern and elders by fellow friends.
-      </Typography>
-      <Button variant="contained" component={Link} to="/register" sx={{ mr: 2 }}>
-        Join Us
-      </Button>
-      <Button variant="outlined" component={Link} to="/about">
-        Learn More
-      </Button>
+    <Box
+  sx={{
+    height: "90vh",                // full viewport height
+    width: "100%",                  // full width
+    backgroundImage: "url('/images/bg.png')",
+    backgroundSize: "100%",      // fit entire image without cropping
+    backgroundPosition: "center",   // center it
+    backgroundRepeat: "no-repeat",  
+    backgroundAttachment: "fixed",  // keeps it fixed (doesn't scroll)
+    display: "fixed",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  }}
+>
+
+      <Box
+  sx={{
+    position: "absolute",
+    bottom: 1,         // distance from bottom
+    left: "50%",
+    transform: "translateX(-50%)",
+    display: "flex",
+    gap: 3              // space between buttons
+  }}
+>
+  <Button
+    variant="contained"
+    component={Link}
+    to="/register"
+    sx={{ color: "white", borderColor: "white" }}
+  >
+    Join Us
+  </Button>
+
+  <Button
+    variant="outlined"
+    component={Link}
+    to="/about"
+    sx={{ color: "white" }}
+  >
+    Learn More
+  </Button>
+</Box>
+  
+      
     </Box>
+    
   );
 };
 
