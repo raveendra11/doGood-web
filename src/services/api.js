918e88f9051c5a5af9dba4c163b8b86f0400c7a7
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-09-11 06:53:09 -0500

    uncomment cloud url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_URL = 'https://dogood-1030922974196.europe-west1.run.app/api/users';
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://dogood-1030922974196.europe-west1.run.app/api';
+//const API_URL = 'http://localhost:8080/api';
 
 export const registerUser = async (userData) => {
   return await axios.post(`${API_URL}/register`, userData);
